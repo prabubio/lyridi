081fd8261f7a13ecec54e5f68cc696aee745de4e
Prabu <praja@yahoo-inc.com>
2014-11-08 17:46:03 -0800

    new song

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  var fs = require("fs");
-      data = fs.readFileSync(__dirname + "/../data/kaththi/aathi_enai.json",'utf8',function (err, data) {
-        if (err) throw err;
-        console.log("test"+data)
-      });
-  data = JSON.parse(data);
-  res.render('index', data);
+/* GET song page. */
+router.get('/:movie/:song', function(req, res) {
+    var fs = require("fs"),
+        movie = req.params.movie,
+        song = req.params.song,
+        parts = song.match("(.*)?\.html$");
+
+    if (parts && parts[1]) {
+        data = fs.readFileSync(__dirname + "/../data/" + movie + "/" + parts[1] + ".json",'utf8',function (err, data) {
+            if (err) throw err;
+            console.log("test"+data);
+        });
+        data = JSON.parse(data);
+        res.render('index', data);
+    } else {
+        res.send("Page not available");
+        console.log("404"+ movie + song);
+    }
 });
 
-router.get('/public/stylesheets/:file', function(req, res) {
-  var file = req.params.file;
-  res.sendfile(__dirname + '/../stylesheets/' + file);
+/* GET home page. */
+router.get('/', function(req, res) {
+    var fs = require("fs");
+    data = fs.readFileSync(__dirname + "/../data/kaththi/aathi_enai.json",'utf8',function (err, data) {
+        if (err) throw err;
+        console.log("test"+data)
+    });
+    data = JSON.parse(data);
+    res.render('index', data);
+});
+
+
+router.get('/public/:type/:file', function(req, res) {
+  var file = req.params.file,
+      type = req.params.type;
+    console.log(__dirname + '/../' + type + '/' + file);
+  res.sendfile(__dirname + '/../' + type + '/' + file);
 });
 
 module.exports = router;
